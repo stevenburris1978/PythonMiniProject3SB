770cbff5597ee70fdace2792380e2aada184983d
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-10-23 00:59:07 -0500

    updated Help and added comments

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -2,7 +2,6 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
-
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
